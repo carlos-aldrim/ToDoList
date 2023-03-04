3744984425d65da425c361c5a9c9e9493248e5f8
Carlos-Melo3 <94694859+Carlos-Melo3@users.noreply.github.com>
2023-03-04 14:34:56 -0300

    Initial commit

---
+++ src/index.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import reportWebVitals from './reportWebVitals';
+import { App } from './components';
+import store from "./store";
+import { Provider } from "react-redux";
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </React.StrictMode>
+);
+
+reportWebVitals();
